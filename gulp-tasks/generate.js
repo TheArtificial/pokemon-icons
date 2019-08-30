89098aad1e1898cf95789c2f4287afc1224649b1
gerwitz <hans@gerwitz.com>
2019-08-30 19:49:09 +0200

    Publishing

--- gulp-tasks/generate.js
+++
@@ -1,7 +0,0 @@
-const gulp    = require('gulp');
-const shell = require('gulp-shell');
-
-/*
- Run our static site generator to build the pages
-*/
-gulp.task('generate', shell.task('npx eleventy --quiet'));
